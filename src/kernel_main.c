5d531652e01c48209d41114df288ba2463e4db2f
Jacob Oh <joh@luc.edu>
2025-09-29 17:06:36 +0000

    I/O

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -11,7 +11,45 @@ uint8_t inb (uint16_t _port) {
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
     return rv;
 }
-
+unsigned char keyboard_map[128] =
+{
+   0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+ '9', '0', '-', '=', '\b',     /* Backspace */
+ '\t',                 /* Tab */
+ 'q', 'w', 'e', 'r',   /* 19 */
+ 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+   0,                  /* 29   - Control */
+ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
+'\'', '`',   0,                /* Left shift */
+'\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
+ 'm', ',', '.', '/',   0,                              /* Right shift */
+ '*',
+   0,  /* Alt */
+ ' ',  /* Space bar */
+   0,  /* Caps lock */
+   0,  /* 59 - F1 key ... > */
+   0,   0,   0,   0,   0,   0,   0,   0,  
+   0,  /* < ... F10 */
+   0,  /* 69 - Num lock*/
+   0,  /* Scroll Lock */
+   0,  /* Home key */
+   0,  /* Up Arrow */
+   0,  /* Page Up */
+ '-',
+   0,  /* Left Arrow */
+   0,  
+   0,  /* Right Arrow */
+ '+',
+   0,  /* 79 - End key*/
+   0,  /* Down Arrow */
+   0,  /* Page Down */
+   0,  /* Insert Key */
+   0,  /* Delete Key */
+   0,   0,   0,  
+   0,  /* F11 Key */
+   0,  /* F12 Key */
+   0,  /* All other keys are undefined */
+};
 /* ------------------------------------------------------------------
    Kernel entry point
    ------------------------------------------------------------------ */
@@ -24,16 +62,18 @@ void main() {
         uint8_t status = inb(0x64);
         if (status & 1) {
             uint8_t scancode = inb(0x60);
-            esp_printf(console_putc, "Key pressed: %02x\n", scancode);
-        }
-
-        // crude delay loop (~1 second depending on CPU speed/QEMU)
-        for (volatile unsigned long i = 0; i < 100000000; i++) {
+            if (scancode > 128) {
+                continue;
+            }
+            esp_printf(console_putc, "Key pressed: %c\n", keyboard_map[scancode]);
+}        // crude delay loop (~1 second depending on CPU speed/QEMU)
+      //  for (volatile unsigned long i = 0; i < 100000000; i++) {
             // just waste time
-        }
+    //    }
 
         // heartbeat message
-        puts("tick...\n");
-
-    }
+  //      puts("tick...\n");
+//
+ //   }
+}
 }
